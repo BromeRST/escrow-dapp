ba32db00a540c14f45ef8c795c03f79a94136585
Massimiliano <massimiliano.albini@gmail.com>
2022-02-18 23:19:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,334 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./utils/Escrow.json";
 
-function App() {
+const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [allContracts, setAllContracts] = useState([]);
+  const [arb, setArb] = useState("");
+  const [ben, setBen] = useState("");
+  const [am, setAm] = useState("");
+
+  const contractAddress = "0x32dC2F7ab31cad1255966cB0489281F9DD05Bb83"
+  const contractABI = abi.abi;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllContracts();
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    const { ethereum } = window;
+    try {
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const addNewContract = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+
+        const contractTxn = await escrowContract.writeNewContract(arb, ben, {value: ethers.utils.parseEther(am), gasLimit: 300000 })
+        console.log("Mining...", contractTxn.hash);
+
+        await contractTxn.wait();
+        console.log("Mined -- ", contractTxn.hash);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getAllContracts = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const contracts = await escrowContract.getAllContracts();
+
+        const contractsCleaned = contracts.map(c => {
+          return {
+            arbiterAddress: c.Arbiter,
+            beneficiaryAddress: c.Beneficiary,
+            depositerAddress: c.Depositor,
+            amount: c.Amount,
+            arbiterApproved: c.ArbiterApproved,
+            escrowApproved: c.IsApproved,
+            ewscowDismissed: c.Dismissed,
+          };
+        });
+
+        contractsCleaned.reverse();
+
+        setAllContracts(contractsCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const approveEscrow = async (i) => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const approveTxn = await escrowContract.approve(i)
+        console.log("Mining...", approveTxn.hash);
+
+        await approveTxn.wait();
+        console.log("Mined -- ", approveTxn.hash);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const approveArbiterFromBeneficiary = async (i) => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+
+        const approveTxn = await escrowContract.approveArbiter(i)
+        console.log("Mining...", approveTxn.hash);
+
+        await approveTxn.wait();
+        console.log("Mined -- ", approveTxn.hash);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const dismissThisEscrow = async (i) => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+
+        const approveTxn = await escrowContract.dismissEscrow(i)
+        console.log("Mining...", approveTxn.hash);
+
+        await approveTxn.wait();
+        console.log("Mined -- ", approveTxn.hash);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    let escrowContract;
+  
+    const onNewContract = (arbiter, beneficiary, depositor, amountEl, arbAppr, isAppr, isDism) => {
+      console.log("NewContract", arbiter, beneficiary, depositor, amountEl, arbAppr, isAppr, isDism);
+      setAllContracts(prevState => [
+        {
+          arbiterAddress: arbiter,
+          beneficiaryAddress: beneficiary,
+          depositerAddress: depositor,
+          amount: amountEl,
+          arbiterApproved: arbAppr,
+          escrowApproved: isAppr,
+          ewscowDismissed: isDism,
+        },
+        ...prevState,
+      ]);
+    };
+  
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+  
+      escrowContract = new ethers.Contract(contractAddress, contractABI, signer);
+      escrowContract.on("NewContract", onNewContract);
+    }
+  
+    return () => {
+      if (escrowContract) {
+        escrowContract.off("NewContract", onNewContract);
+      }
+    };
+  }, []);
+
+  function handleArbiterChange(e) {
+    const {value} = e.target
+    setArb(value)
+  }
+
+  function handleBeneficiaryChange(e) {
+    const {value} = e.target
+    setBen(value)
+  }
+
+  function handleAmountChange(e) {
+    const {value} = e.target
+    setAm(value)
+  }
+
+  function deployButtonClick() {
+    addNewContract();
+    setArb("");
+    setBen("");
+    setAm("");
+  }
+
+  function approveButtonClick(e) {
+    const {id} = e.target
+    approveEscrow(id);
+  }
+
+  function approveArbiterButtonClick(e) {
+    const {id} = e.target
+    approveArbiterFromBeneficiary(id);
+  }
+
+  function dismissButtonClick(e) {
+    const {id} = e.target
+    dismissThisEscrow(id);
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flexi">
+      <div className="contract">
+        {!currentAccount && (
+            <button className="waveButton" onClick={connectWallet}>
+              Connect Wallet
+            </button>
+        )}
+        <h1> New Contract </h1>
+        <label>
+          Arbiter Address
+          <input type="text" value={arb} onChange={handleArbiterChange}/>
+        </label>
+
+        <label>
+          Beneficiary Address
+          <input type="text" value={ben} onChange={handleBeneficiaryChange}/>
+        </label>
+
+        <label>
+          Deposit Amount (in Ether)
+          <input type="text" value={am} onChange={handleAmountChange}/>
+        </label>
+
+        <div className="button" onClick={deployButtonClick}>
+          Deploy
+        </div>
     </div>
-  );
+
+    <div className="existing-contracts">
+      <h1> Existing Contracts </h1>
+
+      <div id="container">
+          {allContracts.length > 0 && 
+            allContracts.map((c, i) => (
+              <div key={i} className="existing-contract">
+                <ul className="fields">
+                  <li>
+                    <div> Arbiter </div>
+                    <div> {c.arbiterAddress} </div>
+                  </li>
+                  <li>
+                    <div> Beneficiary </div>
+                    <div> {c.beneficiaryAddress} </div>
+                  </li>
+                  <li>
+                    <div> Value </div>
+                    <div> {ethers.utils.formatUnits(c.amount)} </div>
+                  </li>
+                  <button className="button btn" id={i} onClick={approveArbiterButtonClick} disabled={c.arbiterApproved || c.escrowDismissed}>
+                  {c.arbiterApproved ? "Arbiter has been approved" : "Arbiter's approve from Beneficiary"}
+                  </button>
+                  <button className="button btn" id={i} onClick={approveButtonClick} disabled={c.escrowApproved || c.escrowDismissed}>
+                    {c.escrowApproved ? "Escrow approved" : "Approve"}
+                  </button>
+                  <button className="button btn" id={i} onClick={dismissButtonClick} disabled={c.escrowApproved || c.escrowDismissed}>
+                    {c.escrowDismissed ? "Escrow had been dismissed" : "Dismiss this escrow"}
+                  </button>
+                </ul>
+            </div>
+            ))
+          }
+      </div>
+    </div>
+  </div>
+  )
+
 }
 
 export default App;
